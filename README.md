0bf84d225ff24ce3d16c1c1161d4776ca390fe70
Takahito Urai <uraitakahito@gmail.com>
2025-01-18 22:25:50 +0900

    Refactor build process: update clean script, modify Rollup config for multiple entry points, and replace chromeExtension plugin with copy plugin

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ```console
 % pnpm store prune
-% pnpm cleandep
+% pnpm clean
 % pnpm install
 % rollup -c -w
 ```
-
-When rollup is in watch mode, [a reload mechanism is built in](https://github.com/crxjs/chrome-extension-tools/blob/508f52f6706a7c875955f77e03309b5c28930127/packages/rollup-plugin/src/plugin-reloader-simple/index.ts#L45-L209). As a result, the extension will be automatically updated when files are changed.
--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,37 @@
-import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension';
 // A Rollup plugin to convert CommonJS modules to ES6
 import commonjs from '@rollup/plugin-commonjs';
 // https://rollupjs.org/command-line-interface/#config-intellisense
 import { defineConfig } from 'rollup';
 // The @rollup/plugin-node-resolve plugin teaches Rollup how to find external modules.
 import resolve from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy'
 
-const config = defineConfig({
-  input: 'src/manifest.json',
-  output: {
-    dir: 'dist',
-    format: 'esm',
-  },
-  plugins: [
-    // always put chromeExtension() before other plugins
-    chromeExtension(),
-
-    simpleReloader(),
-    resolve(),
-    commonjs(),
+//
+// This project has no barrel files and multiple entry points, so defineConfig() accepts an array of configurations
+//
+const config = defineConfig(
+  [
+    {
+      input: 'src/content.js',
+      output: [
+        {
+          dir: 'dist',
+          format: 'esm',
+          preserveModules: true,
+        },
+      ],
+      plugins: [
+        resolve(),
+        commonjs(),
+        copy({
+          targets: [
+            { src: 'src/hello.html', dest: 'dist' },
+            { src: 'src/manifest.json', dest: 'dist' },
+          ],
+        })
+      ],
+    },
   ],
-});
+);
 
 export default config;
